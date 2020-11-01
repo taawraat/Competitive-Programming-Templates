716770fe7ebb8e7420baa2bb2fac8d3125bae508
Zahidul Islam <tawraat@pop-os.localdomain>
2020-11-01 12:53:53 +0600

    string addition added

---
+++ cpp/bigInt.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// string addition
+string sum(string num1,string num2){
+    int len1 = num1.length();
+    int len2 = num2.length();
+    string ans;
+    bool end1 = false, end2 = false;
+    int i = 0, carry = 0;
+    
+    // will break if num1 and num2 ends
+    for( ;!end1 || !end2; ){
+        // iterating in reverse order
+        if(len1) len1--;
+        if(len2) len2--;
+
+        // adding num1 and num2
+        int temp = (num1[len1]-48) + (num2[len2] - 48);
+        temp += carry;
+
+        carry = temp / 10;
+        ans.push_back((temp % 10)+48);
+        i++;
+
+        // initializing to 0 if num1 or num2 ends
+        if(!len1){ end1 = true; num1[len1] = '0'; }
+        if(!len2){ end2 = true; num2[len2] = '0'; }
+    }
+
+    if(carry) ans.push_back(carry+48);
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main(){
+    // sample test case
+    cout << sum("123456","789197699") << endl;
+}
