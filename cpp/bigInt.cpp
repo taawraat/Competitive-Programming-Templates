c45134117324af65e2e06eee7e3d3e5d5ee830ac
Tawraat <tawraat0187@gmail.com>
2020-12-12 15:07:41 +0600

    addition and multiplication with string

--- cpp/bigInt.cpp
+++ cpp/bigInt.cpp
@@ -33,7 +33,35 @@ string sum(string num1,string num2){
     return ans;
 }
 
+//str multiplication of a*b
+string strMultiplication(string a, string b){
+    string ans = "0";
+    string postZeros = "";
+ 
+    for(int i = b.length()-1; i >= 0; i--){
+        int carry = 0;
+        string current;
+ 
+        for(int j = a.length()-1; j >= 0; j--){
+            int S1_j = a[j] - 48;
+            int S2_i = b[i] - 48;
+            int temp = (S1_j * S2_i) + carry;
+            carry = temp/10;
+            current.push_back((char)((temp%10) + 48));
+        }
+        if(carry) current.push_back(carry+48);
+        reverse(current.begin(), current.end());
+        current += postZeros;
+        ans = sum(ans,current);
+        postZeros += "0";
+    }
+ 
+    return ans;
+}
+
+// driver code
 int main(){
     // sample test case
     cout << sum("123456","789197699") << endl;
+    cout << strMultiplication("123456","789197699") << endl;
 }
