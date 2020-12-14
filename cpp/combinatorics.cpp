fd7b7ccdc610e0142c7c4a56c8425954efd23096
Tawraat <tawraat0187@gmail.com>
2020-12-14 19:30:15 +0600

    set divisibility added

--- cpp/combinatorics.cpp
+++ cpp/combinatorics.cpp
@@ -27,6 +27,38 @@ ll nPr(int n, int r){
     return ans;
 }
 
+ll LCM(ll x, ll y){ return x * (y/__gcd(x,y)); }
+
+// returns how many numbers are divisible by Sn from 1 to range
+ll divisibilityOfSet(ll range, ll set[], int elements){
+
+    // subsets = (2^elements) - 1
+    ll subSets = (1<<elements);
+    ll ans = 0;
+
+    // loop from 1 to subsets
+    for(int i = 1; i < subSets; i++){
+        ll devide = 1;
+        ll bitSets = __builtin_popcount(i);
+
+        // searching setbit positions
+        for(int j = 0; j < elements; j++){
+            if(i & (1<<j))
+                devide = LCM(devide, set[j]);
+        }
+
+        // if number of elements is odd then add to ans
+        if(bitSets & 1)
+            ans = ans + (range/devide);
+
+        // if number of elements is even then subtract from ans
+        else
+            ans = ans - (range/devide);
+    }
+    
+    return ans;
+}
+
 // driver function
 int main(){
   // start
