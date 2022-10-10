9eb0dddc53058b6b75f2f76e7d43233d27a3fb65
Tawraat <tawraat0187@gmail.com>
2022-10-10 20:20:37 +0600

    updated matrix expo template

--- cpp/Dynamic Programming/Matrix Exponentiation.cpp
+++ cpp/Dynamic Programming/Matrix Exponentiation.cpp
@@ -1,10 +1,11 @@
 class MatrixExpo{
 public:
     int MAX_N;
-    vector<vector<int>> ans;
+    vector<vector<int>> ans; // give the ans datatype manually
 
-    vector<vector<int>> matMul(vector<vector<int>>& a, vector<vector<int>>& b){
-        vector<vector<int>> res(MAX_N, vector<int>(MAX_N));
+    template<typename T>
+    vector<vector<T>> matMul(vector<vector<T>>& a, vector<vector<T>>& b){
+        vector<vector<T>> res(MAX_N, vector<T>(MAX_N));
         int i, j, k;
 
         for(i = 0; i < MAX_N; i++){
@@ -19,7 +20,8 @@ public:
         return res;
     }
 
-    void matPow(vector<vector<int>>& base, long long power){
+    template<typename T>
+    void matPow(vector<vector<T>>& base, long long power){
         while(power){
             if(power&1) ans = matMul(ans, base);
             base = matMul(base, base);
@@ -27,9 +29,10 @@ public:
         }
     }
 
-    MatrixExpo(vector<vector<int>> base,long long power, int size){
+    template<typename T>
+    MatrixExpo(vector<vector<T>> base,long long power, int size){
         MAX_N = size;
-        ans = vector<vector<int>>(MAX_N, vector<int>(MAX_N));
+        ans = vector<vector<T>>(MAX_N, vector<T>(MAX_N));
 
         for(int i = 0; i < MAX_N; i++){
             for(int j = 0; j < MAX_N; j++){
@@ -40,34 +43,3 @@ public:
         matPow(base, power);
     }
 };
-
-vector<vector<int>> makeBase(vector<int>& v){
-    int sz = v.size();
-    vector<vector<int>> rest(sz, vector<int>(sz));
-
-    for(ll i = 0; i < sz; i++){
-        rest[0][i] = v[i];
-    }
-    for(ll i = 1; i < sz; i++){
-        for(ll j = 0; j < sz; j++){
-            rest[i][j] = (i == (j+1));
-        }
-    }
-
-    /*
-        Example for fibonacci f(n) = f(n-1) + f(n-2)
-                1 1
-                1 0
-        first row = coefficients
-        from second row
-                10000
-                01000
-                00100
-                00010
-                .....
-                .....
-                .....
-    */ 
-
-    return rest;
-}
